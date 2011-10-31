e151150cbee411e187c5a646cc54907de9d6bd8f
akihiro4chawon <akihiro2mb-4jigen@yahoo.co.jp>
2011-10-31 18:31:51 +0900

    removed unused code

--- src/main/scala/gist4z/auth/Auth.scala
+++ src/main/scala/gist4z/auth/Auth.scala
@@ -29,29 +29,3 @@ case class BasicOrDigestIfRequired(user: String, password: String) extends Basic
 case class BasicUnconditionally(user: String, password: String) extends Basic {
   def apply(req: Request) = req.as_!(user, password)
 }
-
-
-
-//import scalaj.http._
-//
-//trait Auth {
-//  def authenticate(req: Http.Request): Http.Request
-//}
-//
-//case object NullAuth extends Auth {
-//  def authenticate(req: Http.Request): Http.Request = req
-//}
-//
-//trait SomeAuth extends Auth
-//
-//case class Basic(val user: String, val password: String) extends SomeAuth {
-//  def authenticate(req: Http.Request) =
-//    req.auth(user, password)
-//}
-//
-//// TODO: revise when Javascript/Desktop Flow is enabled
-//case class OAuth2(val accessToken: String) extends SomeAuth {
-//  def authenticate(req: Http.Request) =
-//    req.param("access_token", accessToken)
-//}
-
